c8da07efa09f3d8e34edd0ace18346ecfb80a120
lofxve <875567313@qq.com>
2021-02-25 19:39:43 +0800

    章节的添加，修改，删除

--- service/service-edu/src/main/java/com/wzd/eduservice/controller/EduChapterController.java
+++ service/service-edu/src/main/java/com/wzd/eduservice/controller/EduChapterController.java
@@ -2,11 +2,13 @@ package com.wzd.eduservice.controller;
 
 
 import com.wzd.commonutils.R;
+import com.wzd.eduservice.entity.EduChapter;
 import com.wzd.eduservice.entity.capter.ChapterVo;
 import com.wzd.eduservice.service.EduChapterService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.poi.openxml4j.opc.PackagingURIHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +36,42 @@ public class EduChapterController {
                              @PathVariable String courseId) {
         List<ChapterVo> chapterVoList = chapterService.getChapterVideo(courseId);
         return R.ok().data("items", chapterVoList);
+    }
+
+    @ApiOperation(value = "添加课程章节")
+    @PostMapping("addChapter")
+    public R addChapter(@ApiParam(name = "EduChapter", value = "章节", required = true)
+                        @RequestBody EduChapter chapter) {
+        chapterService.save(chapter);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "根据id获取课程章节")
+    @GetMapping("getChapter/{chapterId}")
+    public R getChapter(@ApiParam(name = "chapterId", value = "章节id", required = true)
+                        @PathVariable String chapterId) {
+        EduChapter chapter = chapterService.getById(chapterId);
+        return R.ok().data("items", chapter);
+    }
+
+    @ApiOperation(value = "修改课程章节")
+    @PostMapping("updateChapter")
+    public R updateChapter(@ApiParam(name = "courseId", value = "课程id", required = true)
+                           @RequestBody EduChapter chapter) {
+        chapterService.updateById(chapter);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "删除课程章节")
+    @DeleteMapping("deleteChapter/{chapterId}")
+    public R deleteChapter(@ApiParam(name = "chapterId", value = "章节id", required = true)
+                           @PathVariable String chapterId) {
+        boolean flag = chapterService.deleteChapter(chapterId);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
 }
 
--- service/service-edu/src/main/java/com/wzd/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service-edu/src/main/java/com/wzd/eduservice/service/impl/EduChapterServiceImpl.java
@@ -2,6 +2,7 @@ package com.wzd.eduservice.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wzd.baseservice.exceptionHandler.BaseException;
 import com.wzd.eduservice.entity.EduChapter;
 import com.wzd.eduservice.entity.EduVideo;
 import com.wzd.eduservice.entity.capter.ChapterVo;
@@ -29,16 +30,17 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
 
     @Autowired
     private EduVideoMapper videoMapper;
+
     @Override
     public List<ChapterVo> getChapterVideo(String courseId) {
         // 查询所有章节
         QueryWrapper<EduChapter> chapterVoQueryWrapper = new QueryWrapper<>();
-        chapterVoQueryWrapper.eq("course_id",courseId);
+        chapterVoQueryWrapper.eq("course_id", courseId);
         chapterVoQueryWrapper.orderByAsc("sort", "id");
         List<EduChapter> chapters = baseMapper.selectList(chapterVoQueryWrapper);
         // 查询所有小结
         QueryWrapper<EduVideo> videoQueryWrapper = new QueryWrapper<>();
-        videoQueryWrapper.eq("course_id",courseId);
+        videoQueryWrapper.eq("course_id", courseId);
         videoQueryWrapper.orderByAsc("sort", "id");
         List<EduVideo> videos = videoMapper.selectList(videoQueryWrapper);
 
@@ -47,13 +49,13 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
         // 遍历章节
         for (EduChapter chapter : chapters) {
             ChapterVo chapterVo = new ChapterVo();
-            BeanUtils.copyProperties(chapter,chapterVo);
+            BeanUtils.copyProperties(chapter, chapterVo);
             // 遍历小结
             ArrayList<VideoVo> videoVos = new ArrayList<>();
             for (EduVideo video : videos) {
-                if (video.getCourseId().equals(chapter.getCourseId()) && video.getChapterId().equals(chapter.getId())){
+                if (video.getCourseId().equals(chapter.getCourseId()) && video.getChapterId().equals(chapter.getId())) {
                     VideoVo videoVo = new VideoVo();
-                    BeanUtils.copyProperties(video,videoVo);
+                    BeanUtils.copyProperties(video, videoVo);
                     videoVos.add(videoVo);
                 }
             }
@@ -62,5 +64,27 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
             chapterVos.add(chapterVo);
         }
         return chapterVos;
+    }
+
+    /**
+     * @return void
+     * @Author lofxve
+     * @Description // 如果章节下有小结，则不能进行删除
+     * @Date 18:20 2021/2/25
+     * @Param [chapterId]
+     **/
+    @Override
+    public boolean deleteChapter(String chapterId) {
+        // 查询这个章节下的小结
+        QueryWrapper<EduVideo> videoQueryWrapper = new QueryWrapper<>();
+        videoQueryWrapper.eq("chapter_id", chapterId);
+        Integer count = videoMapper.selectCount(videoQueryWrapper);
+        if (count > 0) {
+            throw new BaseException(20001, "该章节下含有小结不能进行删除");
+        } else {
+            // 删除章节
+            int i = baseMapper.deleteById(chapterId);
+            return i > 0;
+        }
     }
 }
