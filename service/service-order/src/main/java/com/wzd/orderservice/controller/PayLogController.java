4794ffbb246fe6d554b25f9ccaa47101d3b6a3f7
lofxve <875567313@qq.com>
2021-03-06 18:08:56 +0800

    创建微信支付二维码

--- service/service-order/src/main/java/com/wzd/orderservice/controller/PayLogController.java
+++ service/service-order/src/main/java/com/wzd/orderservice/controller/PayLogController.java
@@ -1,9 +1,14 @@
 package com.wzd.orderservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.wzd.commonutils.R;
+import com.wzd.orderservice.service.PayLogService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,9 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wzd
  * @since 2021-03-06
  */
+@Api("支付日志表")
+@CrossOrigin
 @RestController
-@RequestMapping("/orderservice/pay-log")
+@RequestMapping("/orderservice/paylog")
 public class PayLogController {
+    @Autowired
+    private PayLogService payLogService;
 
+    @ApiOperation("生成微信支付二维码")
+    @GetMapping("createNative/{orderNo}")
+    public R createNative(@PathVariable String orderNo) {
+        Map map = payLogService.createNative(orderNo);
+        return R.ok().data(map);
+    }
 }
 
--- service/service-order/src/main/java/com/wzd/orderservice/service/impl/PayLogServiceImpl.java
+++ service/service-order/src/main/java/com/wzd/orderservice/service/impl/PayLogServiceImpl.java
@@ -1,10 +1,20 @@
 package com.wzd.orderservice.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.wxpay.sdk.WXPayUtil;
+import com.wzd.orderservice.entity.Order;
 import com.wzd.orderservice.entity.PayLog;
 import com.wzd.orderservice.mapper.PayLogMapper;
+import com.wzd.orderservice.service.OrderService;
 import com.wzd.orderservice.service.PayLogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wzd.orderservice.utils.HttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +26,54 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PayLogServiceImpl extends ServiceImpl<PayLogMapper, PayLog> implements PayLogService {
+    @Autowired
+    private OrderService orderService;
 
+    @Override
+    public Map createNative(String orderNo) {
+        try {
+            //根据订单id获取订单信息
+            QueryWrapper<Order> wrapper = new QueryWrapper<>();
+            wrapper.eq("order_no", orderNo);
+            Order order = orderService.getOne(wrapper);
+
+            Map m = new HashMap();
+            //1、设置支付参数
+            m.put("appid", "wx74862e0dfcf69954");
+            m.put("mch_id", "1558950191");
+            m.put("nonce_str", WXPayUtil.generateNonceStr());
+            m.put("body", order.getCourseTitle());
+            m.put("out_trade_no", orderNo);
+            m.put("total_fee", order.getTotalFee().multiply(new BigDecimal("100")).longValue() + "");
+            m.put("spbill_create_ip", "127.0.0.1");
+            m.put("notify_url", "http://guli.shop/api/order/weixinPay/weixinNotify\n");
+            m.put("trade_type", "NATIVE");
+
+            //2、HTTPClient来根据URL访问第三方接口并且传递参数
+            HttpClient client = new HttpClient("https://api.mch.weixin.qq.com/pay/unifiedorder");
+
+            //client设置参数
+            client.setXmlParam(WXPayUtil.generateSignedXml(m, "T6m9iK73b0kn9g5v426MKfHQH7X8rKwb"));
+            client.setHttps(true);
+            client.post();
+            //3、返回第三方的数据
+            String xml = client.getContent();
+            Map<String, String> resultMap = WXPayUtil.xmlToMap(xml);
+            //4、封装返回结果集
+
+            Map map = new HashMap<>();
+            map.put("out_trade_no", orderNo);
+            map.put("course_id", order.getCourseId());
+            map.put("total_fee", order.getTotalFee());
+            map.put("result_code", resultMap.get("result_code"));
+            map.put("code_url", resultMap.get("code_url"));
+
+            //微信支付二维码2小时过期，可采取2小时未支付取消订单
+            //redisTemplate.opsForValue().set(orderNo, map, 120, TimeUnit.MINUTES);
+            return map;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
 }
